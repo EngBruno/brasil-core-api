3b07a2f29011ac67dac740b30508393f6bb57479
Bruno Roberto <roberto.bruno007@gmail.com>
2018-07-14 09:22:23 -0300

    first commit

---
+++ src/main/java/com/bruno/BrasilCoreApiApplication.java
@@ -0,0 +1,12 @@
+package com.bruno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrasilCoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrasilCoreApiApplication.class, args);
+	}
+}
